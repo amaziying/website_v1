09f57975eb6f9807a145a188772a9bd48ea9bd58
Ziying <ziying95@hotmail.com>
2014-12-05 01:33:28 -0500

    Website mass update
    New features: loading spinner
    Updated content
    Updated resume

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
-//scroll down animation
-
-var $root = $('html, body');
-$('a').click(function() {
-    $root.animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 800);
-    return false;
-});
-
-
 //Fade in letters	 
 	 
-$(document).ready(function(){	
+$(window).on('load', function(){
+
+    $('#loading-spinner').remove();
+    //scroll down animation
+
+    var $root = $('html, body');
+    $('a').click(function() {
+        $root.animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 800);
+        return false;
+    });	
  
-	 var section1 = $('.fadein');
+	var section1 = $('.fadein');
 
-section1.hide().contents().each(function() {
-    var words;
-    if (this.nodeType === 3) {
-        words = '<span> ' + this.data.split(/\s+/).join(' </span><span> ') + ' </span>';
-        $(this).replaceWith(words);
-    } else if (this.nodeType === 1) {
-        this.innerHTML = '<span> ' + this.innerHTML.split(/\s+/).join(' </span><span> ') + ' </span>';
-    }
-});
+    section1.hide().contents().each(function() {
+        var words;
+        if (this.nodeType === 3) {
+            words = '<span> ' + this.data.split(/\s+/).join(' </span><span> ') + ' </span>';
+            $(this).replaceWith(words);
+        } else if (this.nodeType === 1) {
+            this.innerHTML = '<span> ' + this.innerHTML.split(/\s+/).join(' </span><span> ') + ' </span>';
+        }
+    });
 
-section1.find('span').hide().each(function() {
-    if( !$.trim(this.innerHTML) ) {
-        $(this).remove();
-    }
-});
+    section1.find('span').hide().each(function() {
+        if( !$.trim(this.innerHTML) ) {
+            $(this).remove();
+        }
+    });
 
-section1.show().find('span').each(function(i) {
-    $(this).delay(600 * i).fadeIn(600);
-});
+    section1.show().find('span').each(function(i) {
+        $(this).delay(600 * i).fadeIn(600);
+    });
 
-var button1 = $('#btn-anim');
-button1.hide();
-button1.delay(4800).fadeIn(600);
+    var button1 = $('#btn-anim');
+    button1.hide();
+    button1.delay(4800).fadeIn(600);
 
 });
